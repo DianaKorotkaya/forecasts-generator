9b00dfb68f7a213dc2e2c7778c955fe714c8b0ff
DianaKorotkaya <dkorotkaya92@gmail.com>
2024-03-07 23:10:57 +0300

    Домашнее задание-5

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
+const sendButton = document.querySelector('.forecast-btn');
+const conteinerText = document.querySelector('.current-forecast h1');
+const conteinerNum = document.querySelector('.current-forecast p');
+const itemTemplate = document.querySelector('#forecast-item');
+const conteiner = document.querySelector('.forecasts');
+
+function generateRandomValue(min, max) {
+  let predictionNumber = Math.floor(Math.random() * (max-min)) + min;
+  return predictionNumber;
+}
+
+sendButton.addEventListener('click', function(){
+    let num = generateRandomValue(0, 3); 
+    let predictionText = "";
+    let percentage = generateRandomValue(0, 101);
+
+    if (num == 1) {
+      predictionText = "Все будет супер!";
+    } else if (num == 2) {
+      predictionText = "Все будет еще лучше!";
+    } else {
+      predictionText = "На WB отменят все комиссии";
+    }
+
+    function makeItemByTemplate(predictionText,percentage){
+      const myCard = itemTemplate.content.cloneNode(true);
+      myCard.querySelector('h3').textContent = predictionText;
+      myCard.querySelector('p').textContent = 'Вероятность ' + percentage + ' % ';
+      
+      return myCard;
+    }
+   
+    conteiner.prepend(makeItemByTemplate(predictionText,percentage));
+    
+    conteinerText.textContent = predictionText;
+    conteinerNum.textContent = 'Вероятность ' + percentage + ' % ';
+
+  })
+
+
+  
+
+
+
+
+
+  
 
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
 
